c242a3b1573e113e4e37756454ccaf8667e1be32
mullinaxgr2 <mullinaxgr@gmail.com>
2019-03-20 22:39:09 -0400

    nfilter

--- src/main/kotlin/app/commands/Filter.kt
+++ src/main/kotlin/app/commands/Filter.kt
@@ -5,13 +5,15 @@ import org.javacord.api.DiscordApi
 import org.javacord.api.event.message.MessageCreateEvent
 
 class Filter: MessageProcess {
-    private val nRegex: Regex = Regex("n*.gg*.r", RegexOption.IGNORE_CASE)
+    private val nRegex: Regex = Regex("n((i*)|(.{0,4}))g*((e*)|(.{0,4}))r", RegexOption.IGNORE_CASE)
 
     override fun qualifier(event: MessageCreateEvent): Boolean {
-        return nRegex.matches(event.messageContent)
+        return nRegex.containsMatchIn(event.messageContent)
     }
 
     override fun action(event: MessageCreateEvent, api: DiscordApi) {
         event.message.delete()
+        val role = event.server.get().roles.find { r -> r.name == "n-word sayer" }
+        event.messageAuthor.asUser().get().addRole(role)
     }
 }
