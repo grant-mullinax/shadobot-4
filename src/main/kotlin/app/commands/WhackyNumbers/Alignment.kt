6b8a120a830d72ec21291a52197590141c8efc43
mullinaxgr2 <mullinaxgr@gmail.com>
2019-03-24 02:27:35 -0400

    refactored alignment

--- src/main/kotlin/app/commands/WhackyNumbers/Alignment.kt
+++ src/main/kotlin/app/commands/WhackyNumbers/Alignment.kt
@@ -13,18 +13,18 @@ class Alignment: StandardCommand() {
         val id = if (mentionedSomeone) event.messageAuthor.id else event.message.mentionedUsers[0].id
         val targetName = if (mentionedSomeone) "Your" else "${event.message.mentionedUsers[0].name}'s"
         val alignmentCode = id % 9
-        var alignment = "none?"
 
-        when (alignmentCode) {
-            0L -> alignment = "Lawful Good :angel:"
-            1L -> alignment = "Lawful Neutral :zipper_mouth:"
-            2L -> alignment = "Lawful Evil :levitate:"
-            3L -> alignment = "Neutral Good :yum:"
-            4L -> alignment = "True Neutral :expressionless:"
-            5L -> alignment = "Neutral Evil :smirk:"
-            6L -> alignment = "Chaotic Good :upside_down:"
-            7L -> alignment = "Chaotic Neutral :upside_down:"
-            8L -> alignment = "Chaotic Evil :japanese_goblin:"
+        val alignment = when (alignmentCode) {
+            0L -> "Lawful Good :angel:"
+            1L -> "Lawful Neutral :zipper_mouth:"
+            2L -> "Lawful Evil :levitate:"
+            3L -> "Neutral Good :yum:"
+            4L -> "True Neutral :expressionless:"
+            5L -> "Neutral Evil :smirk:"
+            6L -> "Chaotic Good :upside_down:"
+            7L -> "Chaotic Neutral :upside_down:"
+            8L -> "Chaotic Evil :japanese_goblin:"
+            else -> "?"
 
         }
 
