818ef2ae1f3cb321b124a7fee9b057ad2eead54f
mullinaxgr2 <mullinaxgr@gmail.com>
2019-04-13 03:00:21 -0400

    improved image scaling

--- src/main/kotlin/app/commands/Ascii.kt
+++ src/main/kotlin/app/commands/Ascii.kt
@@ -6,6 +6,7 @@ import org.javacord.api.event.message.MessageCreateEvent
 import java.awt.image.BufferedImage
 import java.lang.Float.min
 import java.lang.StringBuilder
+import java.util.*
 import kotlin.math.sqrt
 
 class Ascii: StandardCommand() {
@@ -31,8 +32,10 @@ class Ascii: StandardCommand() {
             val ascii = StringBuilder(".\n`")
 
             val sampledColors = mutableListOf<MutableList<Float>>()
-            var max = 0f
-            var min = (0xff * 3).toFloat()
+
+            var edgeSamples = outputHeight * outputWidth / 200
+            var max = LinkedList<Float>(List(edgeSamples) {0f})
+            var min = LinkedList<Float>(List(edgeSamples) {(0xff * 3).toFloat()})
 
             for (y in 0 until outputHeight) {
                 sampledColors.add(mutableListOf())
@@ -49,19 +52,24 @@ class Ascii: StandardCommand() {
                     val darkness = (red + blue + green).toFloat()
                     sampledColors[y].add(darkness)
 
-                    if (darkness < min) {
-                        min = darkness
-                    } else if (max < darkness) {
-                        max = darkness
+                    if (darkness < min.peekLast()) {
+                        min.push(darkness)
+                        min.sort()
+                        min.removeLast()
+                    } else if (max.peekLast() < darkness) {
+                        max.push(darkness)
+                        max.sortDescending()
+                        max.removeLast()
                     }
                 }
             }
 
-            val colorRange = max - min
+            val minAvg = min.average().toFloat()
+            val colorRange = max.average().toFloat() - minAvg
 
             for (y in 0 until outputHeight) {
                 for (x in 0 until outputWidth) {
-                    val charDistribution = (sampledColors[y][x] - min) / colorRange * (gradient.length - 1)
+                    val charDistribution = (sampledColors[y][x] - minAvg).coerceIn(0f, 1f) / colorRange * (gradient.length - 1)
                     val char = gradient[charDistribution.toInt()]
 
                     ascii.append(char)
