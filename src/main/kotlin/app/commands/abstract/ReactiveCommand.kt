bb58bbab77d49b0d006177e9a45d2a5e9151eb60
mullinaxgr2 <mullinaxgr@gmail.com>
2019-05-04 19:33:35 -0400

    add reactive command

--- src/main/kotlin/app/commands/abstract/ReactiveCommand.kt
+++ src/main/kotlin/app/commands/abstract/ReactiveCommand.kt
@@ -3,6 +3,7 @@ package app.commands.abstract
 import org.javacord.api.event.message.reaction.ReactionAddEvent
 
 abstract class ReactiveCommand : StandardCommand(), ReactionProcess {
+
     override fun qualifier(event: ReactionAddEvent): Boolean {
         return true
     }
