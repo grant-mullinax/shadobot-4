7f76a1888a10de32adc652d8f9d156296060b55c
mullinaxgr2 <mullinaxgr@gmail.com>
2020-04-28 23:14:03 -0400

    refresh gitignore to clean up

--- transfer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sshdir="/c/Users/Grant/.ssh/shadobot-keys.pem"
-jarname="communismbot-1.0-SNAPSHOT.jar"
-jardir="/c/Users/Grant/IdeaProjects/communismbot/flag.png"
-todir="/home/ec2-user/flag.png"
-
-scp -i "$sshdir" "$jardir" ec2-user@ec2-18-188-192-146.us-east-2.compute.amazonaws.com:"$todir"
